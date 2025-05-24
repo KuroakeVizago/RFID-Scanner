9658cc4c92d0483ba139f9ed9f87ba1a7c1c0adf
Zaebi Agustia Hidayatullah <zaebiyt@gmail.com>
2025-05-24 11:25:06 +0700

    [Update] Add beep when scanned

--- RFID_Scanner/RFID_Scanner.ino
+++ RFID_Scanner/RFID_Scanner.ino
@@ -1,28 +1,32 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-// Pin definitions (correspond to D9 and D5 on WEMOS D1 R32)
-#define SS_PIN 5    // SDA connected to D9 (GPIO 5)
-#define RST_PIN 22  // RST connected to D5 (GPIO 22)
+#define SS_PIN 5      // SDA to D9
+#define RST_PIN 22    // RST to D5
+#define BUZZER_PIN 26 // VCC of buzzer to D3 (GPIO 17)
 
-// Create MFRC522 instance
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 void setup() {
-  Serial.begin(115200);       // Initialize serial communications
-  SPI.begin(18, 19, 23, 5);   // SCK=18 (D8), MISO=19 (D6), MOSI=23 (D7), SS=5 (D9)
-  mfrc522.PCD_Init();         // Initialize RFID reader
+  Serial.begin(115200);
+  SPI.begin(18, 19, 23, 5); // SCK, MISO, MOSI, SS
+  mfrc522.PCD_Init();
+
+  pinMode(BUZZER_PIN, OUTPUT);
+  digitalWrite(BUZZER_PIN, LOW); // Keep buzzer off initially
 
   Serial.println("Place an RFID tag near the reader...");
 }
 
 void loop() {
-  // Look for new RFID cards
   if (!mfrc522.PICC_IsNewCardPresent() || !mfrc522.PICC_ReadCardSerial()) {
     return;
   }
 
-  // Show UID on serial monitor
+  // Beep when card is scanned
+  beep();
+
+  // Show UID
   Serial.print("Card UID: ");
   for (byte i = 0; i < mfrc522.uid.size; i++) {
     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
@@ -30,7 +34,12 @@ void loop() {
   }
   Serial.println();
 
-  // Halt communication
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
+}
+
+void beep() {
+  digitalWrite(BUZZER_PIN, HIGH); // Turn buzzer on
+  delay(100);                     // Duration of beep
+  digitalWrite(BUZZER_PIN, LOW);  // Turn buzzer off
 }

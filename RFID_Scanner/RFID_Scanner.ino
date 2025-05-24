cb8a452b2b7d9d5b250e81720547d32599d16029
Zaebi Agustia Hidayatullah <zaebiyt@gmail.com>
2025-05-24 19:59:53 +0700

    LCD Visualization update

--- RFID_Scanner/RFID_Scanner.ino
+++ RFID_Scanner/RFID_Scanner.ino
@@ -33,7 +33,9 @@ void setup() {
 }
 
 void loop() {
+  setTextLCD("Scan a card ^-^");
 
+  // Reinit the RFID Scanner
   SPI.begin(18, 19, 23, 5); // SCK, MISO, MOSI, SS
   mfrc522.PCD_Init();
 
@@ -58,16 +60,34 @@ void loop() {
     cardUID += String(mfrc522.uid.uidByte[i], HEX);
   }
 
-  lcd.init();          // Initialize LCD
-  lcd.setBacklight(HIGH);
-  lcd.setCursor(0, 0); // Column 0, Row 0
-  lcd.print(cardUID);
+  cardUID.toUpperCase();  // Capitalize UID
+  setTextLCD("UID:" + cardUID);
   
   mfrc522.PICC_HaltA();
   mfrc522.PCD_StopCrypto1();
 
-  delay(100);
+  // Delay per scan
+  delay(500);
 }
+
+void setTextLCD(String text) {
+  lcd.init();          // Initialize LCD
+
+  lcd.clear();             // Clear the screen
+  lcd.setCursor(0, 0);     // First line
+
+  if (text.length() <= 16) {
+    lcd.print(text);       // If short, print on first line
+  } else {
+    // Split across two lines
+    lcd.print(text.substring(0, 16));
+    lcd.setCursor(0, 1);   // Second line
+    lcd.print(text.substring(16, min((unsigned int)32, text.length())));
+  }
+
+  lcd.setCursor(0, 2);     // Third line
+}
+
 
 void beep() {
   digitalWrite(BUZZER_PIN, HIGH); // Turn buzzer on
